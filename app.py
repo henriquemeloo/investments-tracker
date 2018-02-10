00b981f6c7ee0d954b91996fbc34e33dda4c04ba
Gabriela Melo <gabimelo10@gmail.com>
2018-02-09 23:51:06 -0200

    get user name from request

--- app.py
+++ app.py
@@ -6,8 +6,8 @@ from controllers import index
 
 app = Flask(__name__)
 
-#client = MongoClient('mongodb://db:27017/')
-client = MongoClient('mongodb://localhost:27017/')
+client = MongoClient('mongodb://db:27017/')
+# client = MongoClient('mongodb://localhost:27017/')
 db = client.test_database
 
 @app.route("/users", methods=['GET', 'POST'])
@@ -18,13 +18,17 @@ def users_route():
             users.append(user.get("name", ""))
         return jsonify({"status": "success", "payload": users})
     elif request.method == 'POST':
-        user_id = db.users.insert_one({'name': 'user name'}).inserted_id
-        return jsonify({"status": "success", "payload": str(user_id)})
+        try:
+            name = request.form["name"]
+            user_id = db.users.insert_one({'name': name}).inserted_id
+            return jsonify({"status": "success", "payload": str(user_id)})
+        except Exception as e:
+            return jsonify({"status": "failed", "payload": "Favor colocar um nome"})
 
 @app.route("/")
 def hello():
     return index()
 
 if __name__ == "__main__":
-    app.run(host="localhost", debug=True)
-    #app.run(host="0.0.0.0", debug=True)
+    # app.run(host="localhost", debug=True)
+    app.run(host="0.0.0.0", debug=True)
