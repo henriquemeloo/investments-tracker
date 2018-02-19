2cd23741eee9ba1cf266cc4753cfa27f8df1e8e7
Henrique Melo <henrimelo10@gmail.com>
2018-02-19 19:54:45 -0300

    Beginning of implementation of Goals functions

--- app.py
+++ app.py
@@ -1,34 +1,31 @@
 from flask import Flask, request, jsonify
 from pymongo import MongoClient
 
-from controllers import index
-#, users
+from controllers import *
 
 app = Flask(__name__)
 
-client = MongoClient('mongodb://db:27017/')
-# client = MongoClient('mongodb://localhost:27017/')
+#client = MongoClient('mongodb://db:27017/')
+client = MongoClient('mongodb://localhost:27017/')
 db = client.test_database
+
+#ROUTES---------------------------------------------------------------------------------------
+
+@app.route("/")
+def hello_route():
+    return hello()
 
 @app.route("/users", methods=['GET', 'POST'])
 def users_route():
-    if request.method == 'GET':
-        users = []
-        for user in db.users.find():
-            users.append(user.get("name", ""))
-        return jsonify({"status": "success", "payload": users})
-    elif request.method == 'POST':
-        try:
-            name = request.form["name"]
-            user_id = db.users.insert_one({'name': name}).inserted_id
-            return jsonify({"status": "success", "payload": str(user_id)})
-        except Exception as e:
-            return jsonify({"status": "failed", "payload": "Favor colocar um nome"})
+    return users()
 
-@app.route("/")
-def hello():
-    return index()
+@app.route("/users/goals", methods=['GET', 'POST'])
+def goals_route():
+    return goals()
+
+
+#---------------------------------------------------------------------------------------------
 
 if __name__ == "__main__":
-    # app.run(host="localhost", debug=True)
-    app.run(host="0.0.0.0", debug=True)
+    app.run(host="localhost", debug=True)
+    #app.run(host="0.0.0.0", debug=True)
--- controllers.py
+++ controllers.py
@@ -1,5 +1,66 @@
-def index():
-	return "Hello Worldd!"
+import datetime
+import calendar
 
-# def users():
-# 	return "These are my users"
+
+def hello():
+	return "Hello World!"
+
+def add_months(source_date, months):
+    month = source_date.month - 1 + months
+    year = source_date.year + month // 12
+    month = month % 12 + 1
+    day = min(source_date.day,calendar.monthrange(year,month)[1])
+    
+    return datetime.date(year, month, day)
+
+def users():
+    if request.method == 'GET':
+        users = []
+        for user in db.users.find():
+            users.append(user.get("name", ""))
+        return jsonify({"status": "success", "payload": users})
+    elif request.method == 'POST':
+        try:
+            name = request.form["name"]
+            user_id = db.users.insert_one({'name': name}).inserted_id
+            return jsonify({"status": "success", "payload": str(user_id)})
+        except Exception as e:
+            return jsonify({"status": "failed", "payload": "Favor colocar um nome"})
+
+def goals():
+	if request.method == 'GET':
+		goals = []
+		for goal in db.goals.find():
+			goals.append(goal.get("name", ""))#COMO INCLUIR TODOS OS DADOS DE CADA GOAL, EM ESPECIAL AS PARCELAS (LISTA)
+		return jsonify({"status": "success", "payload": ""})#????????
+	elif request.method == 'POST':
+		try:
+			name = request.form["name"]
+			price = request.form["price"]
+			end_date = str(request.form["end_date"]) #ESPERADA DATA NO FORMATO MM-YYYY
+			month = int(end_date.rpartition('-')[0])
+			year = int(end_date.rpartition('-')[2])
+			end_date = datetime.date(year, month, 1)
+			start_date = str(request.form["start_date"])
+			month = int(start_date.rpartition('-')[0])
+			year = int(start_date.rpartition('-')[2])
+			start_date = datetime.date(year, month, 1)
+
+
+			for goal in db.goals.find():
+				if goal.get("name") == name:
+					return jsonify({"status": "failed", "payload": "Ja existe objetivo de mesmo nome"})
+
+			num_months = (end_date.year - start_date.year) * 12 + (end_date.month - start_date.month)
+			installments_ids = []
+			for i in range(num_months):
+				value = price/num_months 
+				date = add_months(start_date, i)
+				installment_id = db.installments.insert_one({'value': value, 'paid': "False", 'date': date, 'investment_id': "None"})
+				installments_ids.append(installment_id)
+
+			goal_id = db.goals.insert_one({'name': name, 'price': price, 'end_date': end_date, 'start_date': start_date, 'installments': installments_ids}).inserted_id
+			return jsonify({"status": "success", "payload": str(goal_id)})
+
+		except Exception as e:
+			return jsonify({"status": "failed", "payload": "Error"})
